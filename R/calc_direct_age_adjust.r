a4e580692452015c782545448cb02ad133bfb3b3
injuryepi <injury.epi@gmail.com>
2017-08-05 14:34:37 -0700

    Update calc_direct_age_adjust.r

--- R/calc_direct_age_adjust.r
+++ R/calc_direct_age_adjust.r
@@ -1,4 +1,4 @@
-calc_direct_age_adjust <- function(data, count, population, stdpop = NULL, s = 100000, r = 1, alpha = 0.05 ){
+calc_direct_age_adjust <- function(data, count, population, standard_pop = NULL, s = 100000, r = 1, alpha = 0.05 ){
   # agegrp <- enquo(agegrp)
   count <- enquo(count)
   population <- enquo(population)
@@ -6,17 +6,21 @@ calc_direct_age_adjust <- function(data, count, population, stdpop = NULL, s = 1
   suppressWarnings(suppressMessages(require(tidyverse)))
  
    us2000std = c(0.013818,0.055317,0.145565,0.138646, 0.135573,0.162613,0.134834,0.087247,0.066037,0.044842,0.015508)
-    
+    std_pop <- if (is.null(standard_pop)) {
+      us2000std
+    } else {
+      standard_pop
+    } 
   # calculate adjusted rate and confidence interval with gamma distribution
   # modified from the R package
   # https://cran.r-project.org/web/packages/epitools/index.html 
   # with more method details from
   # https://pdfs.semanticscholar.org/584d/0d020d77e84d193f42e162c59c64795dac6c.pdf
   
-    stdpop <- ifelse(is.null(stdpop), us2000std, stdpop)
+    
     
    rate <- data %>% mutate(rate = count/population) %>% pull(rate)
-    stdwt <- us2000std/sum(stdpop)
+    stdwt <- std_pop/sum(std_pop)
     dsr <- sum(stdwt * rate)
     var_k <- data %>% mutate(var_k = count/population^2) %>% pull(var_k)
     dsr_var <- sum((stdwt^2) * var_k)
