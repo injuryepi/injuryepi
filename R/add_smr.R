3d19444092ffaac647797779333e9d9d0cf3c5b2
injuryepi <injury.epi@gmail.com>
2017-08-06 09:09:06 -0700

    Update add_smr.R

--- R/add_smr.R
+++ R/add_smr.R
@@ -1,7 +1,7 @@
 add_smr <- function(dat, cols, digit = 2, naming = "_smr"){
   # two elements vector of respectively case column and pop column numbers
   # example c(2,3)
-  require(epiR, quietly = T)
+  suppressWarnings(suppressMessages(require(epiR)))
   dat_smr <- round(epiR::epi.conf(as.matrix(dat[, cols]), ctype = "smr"), digits = digit)
   dat_smr <-  subset(dat_smr, select = -se)
   names(dat_smr) <- paste0(names(dat_smr),naming)
