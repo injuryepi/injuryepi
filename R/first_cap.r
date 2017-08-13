dc892db00305ae81a06ece8cbef154fdf8bd1d43
injuryepi <injury.epi@gmail.com>
2017-08-12 21:37:28 -0700

    Update first_cap.r

--- R/first_cap.r
+++ R/first_cap.r
@@ -1,6 +1,2 @@
 
-first_cap <- function(x){
-  suppressWarnings(suppressMessages(require(purrr)))
-  return(compose(function(x) gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", x, perl=TRUE), function(x) tolower(x)))    
-                 }
-
+first_cap <- purrr::compose(function(x) gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", x, perl=TRUE), function(x) tolower(x))
