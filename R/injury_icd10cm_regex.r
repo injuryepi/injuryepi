e79b6787f26075e5f287daeb6c54f7043eec92e0
injuryepi <injury.epi@gmail.com>
2017-08-18 20:17:47 -0700

    Update and rename injury_icd10cm.r to injury_icd10cm_regex.r

--- R/injury_icd10cm_regex.r
+++ R/injury_icd10cm_regex.r
@@ -1,3 +1,3 @@
-injury_icd10cm <- "(?!(^T78|^T8[0-8]))(^S|^T)" 
+injury_icd10cm_regex <- "(?!(^T78|^T8[0-8]))(^S|^T)" 
 # diagnosis code beginning with S or T, except T78 and T80-T88
 # (?!) is  a negative lookhead . It looks the pattern to its right (^S|^T) and excludes (^T78|^T8[0-8])
