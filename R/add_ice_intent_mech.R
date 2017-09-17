61cf8cc792ad8a28a25d701d236c501e569b4ea2
injuryepi <injury.epi@gmail.com>
2017-09-17 14:53:55 -0700

    Add files via upload
    add ice codes data and scripts for injury intent and mechanism

---
+++ R/add_ice_intent_mech.R
@@ -0,0 +1,17 @@
+add_intent <- function(data, underlying_cause){
+
+	underlying_cause <- enquo(underlying_cause)
+	underly <- "underly"
+
+
+	ice_code <- ice_code10 %>%
+		rename(ice_mechanism = ice_name, ice_intent = intent) %>%
+		select(underly, ice_intent, ice_mechanism)
+
+	suppressWarnings(suppressMessages(require(fuzzyjoin)))
+
+	data %>%
+		mutate(!!underly := !!underlying_cause) %>%
+		fuzzyjoin::regex_left_join(ice_code, by = "underly")
+
+}
