71397b26bdce8d71799b4bc9c7cb328f7dbda54b
injuryepi <injury.epi@gmail.com>
2017-12-09 11:06:50 -0800

    Update first_cap.r
    add hyphenated names ("-")

--- R/first_cap.r
+++ R/first_cap.r
@@ -1,2 +1,2 @@
 
-first_cap <- purrr::compose(function(x) gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", x, perl=TRUE), function(x) tolower(x))
+first_cap <- purrr::compose(function(x) gsub("(^|[[:space:]]|-)([[:alpha:]])", "\\1\\U\\2", x, perl=TRUE), function(x) tolower(x))
