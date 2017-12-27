9e87d9ceb38aad8d7c4b6787a343dac301e0b200
injuryepi <injury.epi@gmail.com>
2017-12-27 14:44:03 -0800

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,44 @@
+#' ICD-10-CM codes table and description v2018.
+#'
+#' Dataset of 71,704 rows and 2 variables.
+#'
+#'
+#' @format Data frame
+#' @source
+#'   \url{https://www.cdc.gov/nchs/icd/icd10cm.htm}
+#'
+#' @keywords datasets
+#' @examples
+#' library(dplyr)
+#' sample_n(icd10cm18, 5)
+#'
+"icd10cm18"
+
+#' ICECI codes for ICD10.
+#'
+#' Dataset of 1490rows and 6 variables.
+#'
+#'
+#' @format Data frame
+#'
+#' @keywords reference
+#' @examples
+#' library(dplyr)
+#' sample_n(ice_code10, 5)
+#'
+"ice_code10"
+
+
+#' ICECI codes for ICD10.
+#'
+#' Dataset of 1251 rows and 6 variables.
+#'
+#'
+#' @format Data frame
+#'
+#' @keywords reference
+#' @examples
+#' library(dplyr)
+#' sample_n(ice_code9, 5)
+#'
+"ice_code9"
