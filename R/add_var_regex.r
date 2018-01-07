747eb42c6d71ac0b9e6c2e7748202a0914ff5c4b
injuryepi <injury.epi@gmail.com>
2018-01-06 19:07:06 -0800

    Create add_var_regex.r

---
+++ R/add_var_regex.r
@@ -0,0 +1,26 @@
+add_var_regex <- function(data, expr, var_name, ...) {
+  # regexp=regular expressions for the data colvec = vector of the columns of
+  # interest (columns with the diagnoses)
+  require(dplyr, quietly = T)
+  require(tidyr, quietly = T)
+  require(purrr, quietly = T)
+  
+  # name to assign to the new variable
+  var_name <- enquo(var_name)
+  var_name <- quo_name(var_name)
+  # select the variables of interest
+  sel <- quos(...)
+  
+  #a function to assign '1' if the regular expression matched 
+  f1 <- function(x) as.numeric(grepl(expr, x, ignore.case = ignore.case, perl = perl))
+  f2 <- function(x){as.numeric(rowSums(x, na.rm = TRUE) > 0)} 
+
+  df <- data %>% select(!!!sel) %>% 
+    mutate_all(funs(as.character)) %>% 
+    map_df(f1) %>% 
+    mutate(new_diag = f2(.)) %>% 
+    pull(new_diag)
+  # the vector of the new variable to add to the data 
+  data %>% add_column(!!var_name := df)
+  
+}
