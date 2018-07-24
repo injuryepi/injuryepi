e054d2840bf9cdf396689768af97b43e3901f122
injuryepi <injury.epi@gmail.com>
2018-07-23 18:48:47 -0700

    Update add_ice_intent_mech.r
    by = c(underly = "underly")  changed from by = c("underly" = "underly")

--- R/add_ice_intent_mech.r
+++ R/add_ice_intent_mech.r
@@ -9,7 +9,7 @@ add_ice_intent_mech <- function (data, uid, underlying_cause, ignore_case = TRUE
 		select(underly, ice_intent, ice_mechanism)
 	suppressWarnings(suppressMessages(require(fuzzyjoin)))
 	data %>% mutate(!!underly := !!underlying_cause) %>% 
-		fuzzyjoin::regex_left_join(ice_code, by = c("underly", "underly"), ignore_case = ignore_case) %>% 
+		fuzzyjoin::regex_left_join(ice_code, by = c(underly = "underly"), ignore_case = ignore_case) %>% 
 		filter(!duplicated(!!uid)) %>% 
 	        rename(underly = underly.x) %>%
 	        select(-underly.y)
