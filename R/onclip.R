718b8401d632a2ab5bbb399f74905635d5c27c76
injuryepi <injury.epi@gmail.com>
2018-08-07 11:03:06 -0700

    Add files via upload

--- R/onclip.R
+++ R/onclip.R
@@ -1,3 +1,18 @@
+#' Copy to clipboard. A wrapper to write.table
+#'
+#', reference
+#' @param x input data
+#' @param row.names default to F
+#' @param col.names  default to Tl
+#'
+#' @return table saved to clipboard
+#'
+#' @export
+#' @importFrom
+#'
+#' @examples
+#'
+
 onclip <- function (x, row.names = F, col.names = T)
 {
   write.table(x, file = "clipboard", sep = "\t", row.names = row.names, col.names = col.names)
