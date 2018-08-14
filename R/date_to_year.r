29aa0e6796e4fe3057926f7c028c3a28178d12cf
injuryepi <injury.epi@gmail.com>
2018-08-14 08:47:04 -0700

    Add files via upload

--- R/date_to_year.r
+++ R/date_to_year.r
@@ -1,3 +1,14 @@
+#' Calculate number of years between two dates
+#'
+#' @param date_start: start date
+#' @param date_end: end date
+
+#' @return number of years
+#' @export
+#'
+#' @examples to be added
+
+#'
 date_to_year <- function(date_start, date_end) {
 	suppressWarnings(suppressMessages(require(lubridate)))
 	date_start %--% date_end %/% years(1)
